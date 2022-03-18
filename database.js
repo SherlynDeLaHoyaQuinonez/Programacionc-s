5b51b99a1091597601d94bc55f37920f43c53e03
SherlynDeLaHoyaQuinonez <sherlynhoya92@gmail.com>
2022-03-17 17:49:53 -0700

    primer commit

---
+++ database.js
@@ -0,0 +1,10 @@
+let config = {
+	host: "localhost",
+	user: "root",
+	password: "",
+	port: 3306,
+	database: "adoption_system",
+	secret_key: "ajsdna5jsldnD4"
+
+};
+exports.config = config;
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const usersController = require('../controllers/usersController');
+const userValidator = require('../userValidator');
+const jwtToken = require("../jwtValidation");
+
+router.get('/user', jwtToken.validateToken, userValidator.id, usersController.getUser);
+router.get('/users', jwtToken.validateToken, usersController.getUsers);
+router.post('/user', userValidator.add, usersController.postUser);
+router.post('/login', userValidator.id, usersController.getLogin);
+router.put('/user', userValidator.update, usersController.putUser);
+router.delete('/user', userValidator.id, usersController.deleteUser);
+
+module.exports = router;
